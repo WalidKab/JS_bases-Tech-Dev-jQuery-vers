5c3336ab6cde221d260ce861947501c1dae64e4c
WalidKab <walid.kabli@le-campus-numerique.fr>
2021-04-08 13:18:13 +0200

    Exo 8 OK->télécharger et modifier un plugin

--- JS_bases-Tech-Dev-jQuery-master/exo-8/script.js
+++ JS_bases-Tech-Dev-jQuery-master/exo-8/script.js
@@ -1,1 +1,14 @@
 console.log("exercice 8");
+
+$( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" ).css("background-color", "red")
+          .find( "p" )
+            .html( "Tu as touché la cible, bravo!" );
+      }
+    });
+  } );
+
